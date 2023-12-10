4d72cf026f6d219bc43fb5e6a59b1e5bedf8480c
@hexlet/cli <support@hexlet.io>
2023-12-11 01:58:41 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -6,11 +6,11 @@ import java.util.Map;
 // BEGIN
 public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
+        if (sentence.length == 0) {
+            return (new HashMap<String, Integer>(0));
+
         String[] individualWords = sentence.toLowerCase().split(" ");
         Map<String, Integer> dictionary = new HashMap<>();
-        if (individualWords.length == 0) {
-            return dictionary;
-        }
 
         for (String word : individualWords) {
             Integer counter = dictionary.getOrDefault(word, 0);
@@ -21,15 +21,18 @@ public class App {
     }
 
     public static String toString(Map<String, Integer> catalog) {
+        String emptyLine = "\\{\\}";
         if (catalog.isEmpty()) {
-            return "\\{\\}";
+            return emptyLine.substring(1, emptyLine.length() - 1);
         }
         String catalogText = "";
 
         for (Map.Entry<String, Integer> entry : catalog.entrySet()) {
             catalogText += ("  " + entry.getKey() + ": " + entry.getValue() + "\n");
         }
-        return "\\{\n" + catalogText + "\\}";
+        String fullLine = "\\{\n" + catalogText + "\\}";
+
+        return fullLine.substring(1, fullLine.length() - 1);
     }
 }
 //END
