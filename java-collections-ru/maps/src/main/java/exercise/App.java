b5643c4968a28fda74209b74e96cc16ce2b5b0dd
@hexlet/cli <support@hexlet.io>
2023-12-11 17:21:01 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -1,9 +1,8 @@
 package exercise;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
-// BEGIN
 public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         String[] individualWords = sentence.toLowerCase().split(" ");
@@ -11,29 +10,30 @@ public class App {
 
         if (sentence.isEmpty()) {
             return dictionary;
+        }
 
         for (String word : individualWords) {
             Integer counter = dictionary.getOrDefault(word, 0);
             dictionary.put(word, counter + 1);
         }
-
         return dictionary;
     }
 
-    public static String toString(Map<String, Integer> dictionary2) {
+    public static String toString(Map<String, Integer> dictionary) {
         String emptyLine = "{}";
-        String dictionaryText = "";
-        
-        if (dictionary2.isEmpty()) {
+
+        if (dictionary.isEmpty()) {
             return emptyLine;
         }
 
-        for (Map.Entry<String, Integer> entry : dictionary2.entrySet()) {
-            dictionaryText = dictionaryText + ("  " + entry.getKey() + ": " + entry.getValue() + "\n");
+        StringBuilder dictionaryText = new StringBuilder();
+
+        for (Map.Entry<String, Integer> entry : dictionary.entrySet()) {
+            dictionaryText.append("  ").append(entry.getKey()).append(": ")
+                    .append(entry.getValue()).appand("\n");
         }
         String fullLine = "{\n" + dictionaryText + "}";
 
         return fullLine;
     }
 }
-//END
